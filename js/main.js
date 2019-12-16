9795b2b42ad23bc12c523786dfbd30299de584e2
Adebayo <adebayooriyomi@gmail.com>
2019-12-16 13:29:30 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn_burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav_item');
+const allLinks = document.querySelectorAll('.menu-nav_link')
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu){
+        openNav()
+    }else{
+       closeNav()
+    }
+}
+
+const openNav = () => {
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItems.forEach(item => item.classList.add('open'));
+    showMenu = true;
+}
+
+const closeNav = () => {
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItems.forEach(item => item.classList.remove('open'));
+    showMenu = false;
+}
+// close Nav when Link is clicked
+allLinks.forEach((item) => {
+    item.addEventListener('click', function(e){
+        closeNav()
+    })
+})
